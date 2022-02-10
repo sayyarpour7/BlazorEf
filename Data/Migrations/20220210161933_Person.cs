864c78e48accaeeb3e218a281f356a407adc73f0
pcman99 <pcman99@DESKTOP-INP4JAR>
2022-02-10 23:44:26 +0330

    Add project files.

---
+++ Data/Migrations/20220210161933_Person.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorEf.Data.Migrations
+{
+    public partial class Person : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Data/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEf.Data
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string FirstName { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
